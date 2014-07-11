4ea741767e24ec6e1410667764b6bd6210771e09
patso23 <patso.oshea@ptel.com>
2014-07-11 16:56:05 -0500

    File uploads working, parsing and saving next.

---
+++ webroot/js/lines.js
@@ -0,0 +1,17 @@
+$(function()
+{
+
+ $('#upload').dialog(
+    {
+        title: 'Attach File',
+        autoOpen: false,
+        modal: false,
+        width: 350,
+        height: 200,
+        resizable: false
+    });
+
+
+
+
+});
